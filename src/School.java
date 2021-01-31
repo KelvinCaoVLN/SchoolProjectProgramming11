b15da5b6cfb3f09ad19189c2c2be6d547d502251
Kelvin <Kelvincao.x@gmail.com>
2021-01-31 13:41:10 -0800

    Added setters, getters, and school constructor

--- src/School.java
+++ src/School.java
@@ -1,6 +1,38 @@
 import java.util.ArrayList;
 public class School {
+    School() {
+        this.teachers = teachers;
+        this.students = students;
+        this.subjects = subjects;
+    }
+
     ArrayList<Teacher> teachers = new ArrayList<>();
+
+    public ArrayList<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(ArrayList<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
     ArrayList<Student> students = new ArrayList<>();
+
+    public ArrayList<String> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(ArrayList<String> subjects) {
+        this.subjects = subjects;
+    }
+
     ArrayList<String> subjects =  new ArrayList<>();
 }
